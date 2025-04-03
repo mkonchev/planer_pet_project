4f2e651ba65cd48cad5e72b6e9140749a99b06c7
mkonchev <maksim1901m@gmail.com>
2025-04-03 16:45:43 +0300

    Order fix

--- apps/order/models/OrderModel.py
+++ apps/order/models/OrderModel.py
@@ -2,6 +2,13 @@ from django.db import models
 from apps.order.models.admins.OrderModelAdmin import OrderModelAdmin
 from apps.order.models.consts import TaskPaymentTypeChoice, TaskStatusChoice
 
+class PaymentInfo(models.Model):
+    created_at = models.DateTimeField(verbose_name='Время заказа', auto_now_add=True)
+    pay_at = models.DateTimeField(verbose_name='Время подтверждения оплаты', auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
 class Order(models.Model):
     ModelAdmin = OrderModelAdmin
 
@@ -9,8 +16,6 @@ class Order(models.Model):
     payment_type = models.PositiveIntegerField(verbose_name='Тип оплаты', choices=TaskPaymentTypeChoice, default=TaskPaymentTypeChoice.sbp)
     payment_external_id = models.PositiveIntegerField(verbose_name='ID платежа')
     status = models.PositiveIntegerField(verbose_name='Статус оплаты', choices=TaskStatusChoice, default=TaskStatusChoice.none)
-    created_at = models.DateTimeField(verbose_name='Время заказа')
-    pay_at = models.DateTimeField(verbose_name='Время подтверждения оплаты')
 
     class Meta:
         verbose_name = 'Заказ'
