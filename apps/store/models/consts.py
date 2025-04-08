6a69333ae37076d7bf7e9c7fc467eebb94d4b788
mkonchev <maksim1901m@gmail.com>
2025-04-08 12:46:49 +0300

    add Store choice

--- apps/store/models/consts.py
+++ apps/store/models/consts.py
@@ -1,6 +1,6 @@
 from django.db import models
 
+
 class StoreCategoryChoice(models.IntegerChoices):
-    apps = 0, 'Приложение'
-    #minutes = 1, 'Минуты'...
-    
+    apps = 0, 'Минуты в приложениях'
+    habits = 1, 'Вредные привычки'
