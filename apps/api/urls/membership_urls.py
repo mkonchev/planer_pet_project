697d08a014943648d18844a0ee41f34eb1f3bf94
mkonchev <maksim1901m@gmail.com>
2025-04-19 11:12:53 +0300

    add Membership get_by_id

--- apps/api/urls/membership_urls.py
+++ apps/api/urls/membership_urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from apps.api.views.membership_views import membership_list_view
+from apps.api.views.membership_views import (
+    membership_by_id, membership_list_view,
+)
 
 urlpatterns = [
      path('', membership_list_view),
+     path('<int:pk>', membership_by_id),
 ]
--- apps/api/views/membership_views.py
+++ apps/api/views/membership_views.py
@@ -11,3 +11,10 @@ def membership_list_view(request):
     membership_list = Membership.objects.all()
     serializer = MembershipSerializer(membership_list, many=True)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def membership_by_id(request, pk):
+    membership = Membership.objects.get(pk=pk)
+    serializer = MembershipSerializer(membership)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
