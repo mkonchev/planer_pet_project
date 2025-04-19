f702b8fc14dd576be1a0a77c1e98bdaa8f1d9ef4
mkonchev <maksim1901m@gmail.com>
2025-04-19 12:35:38 +0300

    add User get_by_id, get_by_username

--- apps/api/urls/user_urls.py
+++ apps/api/urls/user_urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from apps.api.views.user_views import user_list_view
+from apps.api.views.user_views import (  # user_by_username,
+    user_by_id, user_list_view,
+)
 
 urlpatterns = [
      path('', user_list_view),
+     path('<int:pk>', user_by_id),
+     # path('<str:username>', user_by_username),
 ]
--- apps/api/views/user_views.py
+++ apps/api/views/user_views.py
@@ -11,3 +11,17 @@ def user_list_view(request):
     user_list = User.objects.prefetch_related('group_members').all()
     serializer = UserSerializer(user_list, many=True)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def user_by_id(request, pk):
+    user = User.objects.get(pk=pk)
+    serializer = UserSerializer(user)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+# @api_view(['GET'])
+# def user_by_username(request, username):
+#     user = User.objects.get(username=username)
+#     serializer = UserSerializer(user)
+#     return Response(data=serializer.data, status=status.HTTP_200_OK)
