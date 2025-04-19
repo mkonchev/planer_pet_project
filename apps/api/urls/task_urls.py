74e75fd4671dbfa5de791b0f877db6a0a0335637
mkonchev <maksim1901m@gmail.com>
2025-04-19 12:30:46 +0300

    add Task get_by_id

--- apps/api/urls/task_urls.py
+++ apps/api/urls/task_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.api.views.task_views import task_list_view
+from apps.api.views.task_views import task_by_id, task_list_view
 
 urlpatterns = [
      path('', task_list_view),
+     path('<int:pk>', task_by_id),
 ]
--- apps/api/views/task_views.py
+++ apps/api/views/task_views.py
@@ -11,3 +11,10 @@ def task_list_view(request):
     task_list = Task.objects.all()
     serializer = TaskSerializer(task_list, many=True)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def task_by_id(request, pk):
+    task = Task.objects.get(pk=pk)
+    serializer = TaskSerializer(task)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
