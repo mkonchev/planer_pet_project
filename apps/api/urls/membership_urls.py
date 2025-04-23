848d6eef1652dbd7b57db19811741bc46cbb08f8
mkonchev <maksim1901m@gmail.com>
2025-04-23 18:04:42 +0300

    add Membership CRUD methods

--- apps/api/urls/membership_urls.py
+++ apps/api/urls/membership_urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from apps.api.views.membership_views import (
-    membership_by_id, membership_list_view,
+    add_membership, delete_membership, membership_by_id, membership_list_view,
+    update_membership,
 )
 
 urlpatterns = [
      path('', membership_list_view),
      path('<int:pk>', membership_by_id),
+     path('create', add_membership),
+     path('<int:pk>/update', update_membership),
+     path('<int:pk>/delete', delete_membership),
 ]
--- apps/api/views/membership_views.py
+++ apps/api/views/membership_views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -18,3 +18,36 @@ def membership_by_id(request, pk):
     membership = Membership.objects.get(pk=pk)
     serializer = MembershipSerializer(membership)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def add_membership(request):
+    membership = MembershipSerializer(data=request.data)
+
+    if membership.is_valid():
+        membership.save()
+        return Response(data=membership.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def update_membership(request, pk):
+    membership = Membership.objects.get(pk=pk)
+    upd_membership = MembershipSerializer(instance=membership, data=request.data)
+
+    if upd_membership.is_valid():
+        upd_membership.save()
+        return Response(data=upd_membership.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_membership(request, pk):
+    if Membership.objects.filter(pk=pk).exists():
+        membership = Membership.objects.get(pk=pk)
+        membership.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+    else:
+        raise serializers.ValidationError('Not found this data')
