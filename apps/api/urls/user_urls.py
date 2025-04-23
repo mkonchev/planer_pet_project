ea89bf3b5f80d2b6440552481e58e1a89bc17a6a
mkonchev <maksim1901m@gmail.com>
2025-04-23 17:43:56 +0300

    add User CRUD methods

--- apps/api/urls/user_urls.py
+++ apps/api/urls/user_urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from apps.api.views.user_views import (  # user_by_username,
-    user_by_id, user_list_view,
+    add_user, delete_user, update_user, user_by_id, user_list_view,
 )
 
 urlpatterns = [
      path('', user_list_view),
      path('<int:pk>', user_by_id),
      # path('<str:username>', user_by_username),
+     path('create', add_user),
+     path('<int:pk>/update', update_user),
+     path('<int:pk>/delete', delete_user),
 ]
--- apps/api/views/user_views.py
+++ apps/api/views/user_views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -25,3 +25,39 @@ def user_by_id(request, pk):
 #     user = User.objects.get(username=username)
 #     serializer = UserSerializer(user)
 #     return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def add_user(request):
+    user = UserSerializer(data=request.data)
+
+    # if user.objects.filter(**request.data).exists():
+    #     raise serializers.ValidationError('This data already exists')
+
+    if user.is_valid():
+        user.save()
+        return Response(data=user.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def update_user(request, pk):
+    user = User.objects.get(pk=pk)
+    upd_user = UserSerializer(instance=user, data=request.data)
+
+    if upd_user.is_valid():
+        upd_user.save()
+        return Response(data=upd_user.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_user(request, pk):
+    if User.objects.filter(pk=pk).exists():
+        user = User.objects.get(pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+    else:
+        raise serializers.ValidationError('Not found this data')
