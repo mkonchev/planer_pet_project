c6b790ff43f15bb0f9703f9e63f9e37c3a84198b
mkonchev <maksim1901m@gmail.com>
2025-04-23 17:56:40 +0300

    add Store CRUD methods

--- apps/api/urls/store_urls.py
+++ apps/api/urls/store_urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from apps.api.views.store_views import store_by_id, store_list_view
+from apps.api.views.store_views import (
+    add_store, delete_store, store_by_id, store_list_view,
+)
 
 urlpatterns = [
      path('', store_list_view),
      path('<int:pk>', store_by_id),
+     path('create', add_store),
+     path('<int:pk>/update', add_store),
+     path('<int:pk>/delete', delete_store),
 ]
--- apps/api/views/store_views.py
+++ apps/api/views/store_views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -18,3 +18,36 @@ def store_by_id(request, pk):
     store = Store.objects.get(pk=pk)
     serializer = StoreSerializer(store)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def add_store(request):
+    store = StoreSerializer(data=request.data)
+
+    if store.is_valid():
+        store.save()
+        return Response(data=store.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def update_store(request, pk):
+    store = Store.objects.get(pk=pk)
+    upd_store = StoreSerializer(instance=store, data=request.data)
+
+    if upd_store.is_valid():
+        upd_store.save()
+        return Response(data=upd_store.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_store(request, pk):
+    if Store.objects.filter(pk=pk).exists():
+        store = Store.objects.get(pk=pk)
+        store.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+    else:
+        raise serializers.ValidationError('Not found this data')
