c4fdd05f8b959d9b626191c4a97798afca23cc33
mkonchev <maksim1901m@gmail.com>
2025-04-23 18:00:49 +0300

    add Order CRUD methods

--- apps/api/urls/order_urls.py
+++ apps/api/urls/order_urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from apps.api.views.order_views import order_by_id, order_list_view
+from apps.api.views.order_views import (
+    add_order, delete_order, order_by_id, order_list_view, update_order,
+)
 
 urlpatterns = [
      path('', order_list_view),
      path('<int:pk>', order_by_id),
+     path('create', add_order),
+     path('<int:pk>/update', update_order),
+     path('<int:pk>/delete', delete_order),
 ]
--- apps/api/views/order_views.py
+++ apps/api/views/order_views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -18,3 +18,36 @@ def order_by_id(request, pk):
     order = Order.objects.get(pk=pk)
     serializer = OrderSerializer(order)
     return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def add_order(request):
+    order = OrderSerializer(data=request.data)
+
+    if order.is_valid():
+        order.save()
+        return Response(data=order.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+def update_order(request, pk):
+    order = Order.objects.get(pk=pk)
+    upd_order = OrderSerializer(instance=order, data=request.data)
+
+    if upd_order.is_valid():
+        upd_order.save()
+        return Response(data=upd_order.data, status=status.HTTP_200_OK)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['DELETE'])
+def delete_order(request, pk):
+    if Order.objects.filter(pk=pk).exists():
+        order = Order.objects.get(pk=pk)
+        order.delete()
+        return Response(status=status.HTTP_202_ACCEPTED)
+    else:
+        raise serializers.ValidationError('Not found this data')
